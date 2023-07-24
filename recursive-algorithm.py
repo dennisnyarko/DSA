2ff5914898389b063116cc0493e355e6e1821d8c
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-07-24 23:45:43 +0000

    first commit

---
+++ recursive-algorithm.py
@@ -0,0 +1,17 @@
+# calculate the factorial of a number
+# def iterative_factorial(n):
+#     fact = 1
+#     for i in range(2, n + 1):
+#         fact *= i
+#     return fact
+# print(iterative_factorial(5))
+
+def recur_factorial(n):
+    if n == 1:
+        return n
+    else:
+        temp = recur_factorial(n-1)
+        temp = temp * n
+    return temp
+
+print(recur_factorial(5))
